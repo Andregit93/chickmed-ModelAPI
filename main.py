921d69383abb9fc29c0961ab0227f06b437a1cdd
Andregit93 <andresevtian501@gmail.com>
2023-12-08 21:39:53 +0700

    add image MD to bucket

--- main.py
+++ main.py
@@ -2,11 +2,35 @@ from flask import Flask, request, jsonify
 import tensorflow as tf
 import keras_cv
 from function import *
+import io
+from PIL import Image
 
 app = Flask(__name__)
 
 @app.route('/predict', methods=['POST'])
 def predict():
+
+    bucket_name = "chickmedbuckets"
+    destination_blob_filename = "chickmed/prediction3.jpg"
+    project_name = "chickmed"
+
+    # Configure bucket and blob
+    client = storage.Client(project=project_name)
+    bucket = client.bucket(bucket_name)
+
+    # Load the image from the POST request
+    im = request.files['image']
+
+    im = Image.open(im)
+
+    # Convert image to bytes
+    bs = io.BytesIO()
+    im.save(bs, "JPEG")
+    bs.seek(0)
+
+    blob = bucket.blob(destination_blob_filename)
+    blob.upload_from_file(bs, content_type="image/jpeg")
+
     # GCP Storage settings
     gcp_bucket_name = 'chickmedbuckets'
     gcp_blob_name = 'chickmed/prediction3.jpg'
